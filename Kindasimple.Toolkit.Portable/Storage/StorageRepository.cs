a408ef5ef08e1ad7c7718992b7769c674ebc5425
Evan White <kindasimple@gmail.com>
2012-12-29 15:10:48 -0500

    some interface changes and updates to packages

---
+++ Kindasimple.Toolkit.Portable/Storage/StorageRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kindasimple.Toolkit.Storage
+{
+    public interface StorageRepository
+    {
+        Task<T> ReadAsync<T>(string key);
+        Task<bool> WriteAsync<T>(string key, T data);
+
+        bool Exists(string key);
+    }
+}
